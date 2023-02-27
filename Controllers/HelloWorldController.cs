c686a110cceb6942ab21f7043ed2b1f5517241c0
Zikkq <zikkq@mail.ru>
2023-02-27 21:33:27 +0100

    Initial

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int id = 1) => HtmlEncoder.Default.Encode($"Hello, {name}. ID: {id}");
+    }
+}
